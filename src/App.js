fd5864a08ea7b3b2b65045089695a748d3896a15
Rizqy Fachri <rizqyfachri@gmail.com>
2023-01-11 09:15:05 +0700

    finish desktop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import "./App.css";
 import rvLogo from "./img/official-logo.png";
+import background from "./img/background.png";
 
 function App() {
   return (
     <div className="wrapper">
       <section className="navbar">
-        <img src={rvLogo} alt="Recycling Village logo" />
+        <img className="logoImg" src={rvLogo} alt="Recycling Village logo" />
       </section>
-      <section className="content">
-        <h1 className="title">RV Website is coming soon</h1>
-        <h2 className="sub-title">
-          Anyway, thanks for checking. In the meantime, you can follow our
-          Instagram.
-        </h2>
-        <button className="instagram-cta">Follow our Instagram</button>
+      <section
+        className="content flex flex-col justify-center gap-y-12"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <section className="words flex flex-col">
+          <h1 className="title sm:text-sm text-headingOne text-center font-bold ">
+            RV Website is coming soon :)
+          </h1>
+
+          <h2 className="sub-title text-headingThree text-center">
+            In the meantime, you can follow us on Instagram or chat with us
+            through WhatsApp
+          </h2>
+        </section>
+        <section className="buttons flex justify-center gap-x-12">
+          <button className="button instagram-cta flex items-center gap-x-4 bg-sun font-bold">
+            <i class="fa-brands fa-instagram"></i>Follow us
+          </button>
+          <button className="button whatsapp-cta flex items-center gap-x-4 bg-sun font-bold">
+            <i class="fa-brands fa-whatsapp"></i>Chat with us
+          </button>
+        </section>
       </section>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'primary': "#000000",
+      'secondary': "#FCF5EE",
+      'aire': "#4472B7",
+      'village': "#ED3724",
+      'earth': "#6CBD45",
+      'sun': "#F5EA07",
+      'hopeful': "#D8C5E0",
+      'white': "#ffffff",
+    },
+    fontFamily: {
+      'poppins': "['Poppins', 'sans-serif']",
+      'ibarra': "['Ibarra Real Nova', 'sans-serif']"
+    },
+    fontSize: {
+      'headingOne': '72px',
+      'headingTwo': '56px',
+      'headingThree': '32px',
+      'headingFour': '26px',
+      'bodyOne': '22px',
+      'bodyTwo': '16px'
+    },
+    extend: {},
+  },
+  plugins: [],
+};
