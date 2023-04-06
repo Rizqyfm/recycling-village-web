2688ef452fdd13cc12e482afd060d9b9a9f61a50
Rizqy Fachri <rizqyfachri@gmail.com>
2023-04-06 14:08:04 +0700

    fix  typograph

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -11,7 +11,7 @@ function Home() {
     <div className="home-wrapper">
       {/* Hero section */}
       <section
-        className="hero-wrapper flex flex-col justify-center items-center gap-y-12 bg-white px-8 md:px-10 lg:px-14 h-screen bg-aire"
+        className="hero-wrapper flex flex-col justify-center items-center gap-y-12 bg-white px-8 md:px-10 lg:px-14 py-32 lg:py-0 lg:h-screen bg-aire"
         style={{
           backgroundImage: `url(${background})`,
           backgroundSize: "cover",
@@ -27,11 +27,19 @@ function Home() {
             RV is partnering with WWF
             <i class="fa-solid fa-arrow-right ml-3 text-white"></i>
           </a> */}
-          <h1 className="title text-bodyOne lg:text-headingOne text-center font-semibold">
-            Crafting trash into treasure, <br />
-            one stitch at a time
+          <h1 className="title text-headingThree md:text-headingTwo lg:text-headingOne lg:hidden text-center font-semibold">
+            Crafting trash into treasure, one stitch at a time
           </h1>
-          <h2 className="sub-title text-bodyThree md:text-bodyOne lg:text-bodyOne text-center text-white">
+          <h1 className="title title-desktop hidden lg:block text-headingThree md:text-headingTwo lg:text-headingOne text-center font-semibold">
+            Crafting trash into treasure,
+            <br /> one stitch at a time
+          </h1>
+          <h2 className="sub-title text-center text-white text-bodyTwo md:text-bodyOne lg:text-headingFour lg:hidden ">
+            Recycling Village is not just a brand, it’s a movement that empowers
+            people to act and make a difference. Join us on our mission to save
+            Mother Earth and All Mothers in rural villages of Indonesia.
+          </h2>
+          <h2 className="sub-title sub-title-destkop hidden lg:block text-bodyTwo md:text-bodyOne lg:text-headingFour text-center text-white">
             Recycling Village is not just a brand, it’s a movement that empowers
             people to act and make a difference. <br />
             Join us on our mission to save Mother Earth and All Mothers in rural
@@ -58,17 +66,17 @@ function Home() {
       <section className="second-wrapper flex flex-col lg:flex-row justify-start items-center bg-hopeful">
         <section className="second-left w-full lg:w-3/5">
           <img
-            className="product-overview w-full h-auto"
+            className="product-overview w-full"
             src={productCatalogue}
             alt="product-overview"
           ></img>
         </section>
-        <section className="second-right flex flex-col items-center md:items-start justify-center gap-y-8 px-8 py-8 md:px-10 lg:px-14">
-          <h2 className="title text-bodyOne text-primary text-center md:text-left lg:text-left lg:text-headingThree">
+        <section className="second-right flex flex-col items-center md:items-start justify-center gap-y-8 px-4 py-8 md:px-10 lg:px-14">
+          <h2 className="title text-headingFour text-primary text-center md:text-left lg:text-left lg:text-headingTwoHalf">
             Crafted with love, <br />
             by women in rural villages of Indonesia
           </h2>
-          <h3 className="sub-title text-bodyTwo text-center md:text-left lg:text-left lg:text-bodyOne">
+          <h3 className="sub-title text-bodyTwo text-center md:text-left lg:text-left lg:text-headingFour">
             One-of-a-kind functional responsible piece of art.
           </h3>
           <a
@@ -102,10 +110,10 @@ function Home() {
           // change image opacity using new image
         }}
       >
-        <h2 className="title text-bodyOne lg:text-headingThree text-center">
+        <h2 className="title text-headingFour lg:text-headingTwoHalf text-center">
           Partnership
         </h2>
-        <h3 className="sub-title text-bodyTwo lg:text-bodyOne text-center text-white">
+        <h3 className="sub-title text-bodyTwo lg:text-headingFour text-center text-white">
           It takes a village to drive change. <br /> Connect with us for
           partnership and collaboration that put The Planet & The People first.
         </h3>
